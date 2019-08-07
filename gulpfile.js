142fbf6469c3395eadeca191657a8d30b912b873
KaloyanTodorov <kaloyanpetev@gmail.com>
2019-08-07 18:04:31 +0300

    Added Scss preprocessor integration.

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,10 @@ let gulp = require("gulp"),
 	cp = require("child_process");
 
 gulp.task("css", function() {
-	return gulp.src( '_assets/css/**/*.css' )
-		// .pipe( sass().on('error', sass.logError) )
+	return gulp.src( '_assets/scss/**/*.scss' )
+		.pipe( sass().on('error', sass.logError) )
 		.pipe( autoprefixer() )
-		.pipe( gulp.dest( './docs/css' ) )
+		.pipe( gulp.dest( './docs/css/' ) )
 		.pipe( browserSync.stream({ match: '**/*.css' }) )
 	;
 });
@@ -36,7 +36,7 @@ gulp.task("watch", function() {
 		}
 	});
 
-	gulp.watch( '_assets/css/**/*.css', gulp.series('css') );
+	gulp.watch( '_assets/scss/**/*.scss', gulp.series('css') );
 
 	gulp.watch(
 		[
